6c9459fe62dc6db96fd7638d6bdcf8bed2735a54
Sai Teja Sree <ponnagantisaitejasree@gmail.com>
2026-03-27 16:14:01 +0530

    Time: 30 ms (64.61%), Space: 133.2 MB (42.48%) - LeetHub

--- 2906-construct-product-matrix/2906-construct-product-matrix.cpp
+++ 2906-construct-product-matrix/2906-construct-product-matrix.cpp
@@ -1,48 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> constructProductMatrix(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-       vector<long> array;
-       for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            array.push_back(grid[i][j]);
-        }
-       }
-       vector<long long>prefix(array.size());
-       long long pre=1;
+        int m = grid.size();
+        int n = grid[0].size();
+        const int mod = 12345;
 
-       vector<long long>suffix(array.size());
-       long long suf=1;
+        int size = m * n;
+        vector<int> res(size);
 
-       for(int k=0;k<array.size();k++){
-        
-        if(k!=0){
-            pre = (pre * (array[k-1] % 12345)) % 12345;
-            prefix[k]=pre;
+        // Step 1: prefix
+        long long pre = 1;
+        for(int i = 0; i < size; i++) {
+            res[i] = pre;
+            pre = (pre * grid[i / n][i % n]) % mod;
         }
-        else if(k==0){
-            prefix[k]=1;
-        }
-       }
-       for(int l=array.size()-1;l>=0;l--){
-        if(l!=array.size()-1){
-            suf=(suf*(array[l+1]%12345))%12345;
-            suffix[l]=suf;
-        }
-        else if(l==array.size()-1){
-            suffix[l]=1;
-        }
-       }
-       for(int i=0;i<array.size();i++){
-        array[i]=(prefix[i]*suffix[i])%12345;
-       }
-       for(int a=0;a<m;a++){
-        for(int b=0;b<n;b++){
-            grid[a][b]=array[a*n+b];
-        }
-       }
-       return grid;
 
+        // Step 2: suffix
+        long long suf = 1;
+        for(int i = size - 1; i >= 0; i--) {
+            res[i] = (res[i] * suf) % mod;
+            suf = (suf * grid[i / n][i % n]) % mod;
+        }
+
+        // Step 3: put back into grid
+        for(int i = 0; i < size; i++) {
+            grid[i / n][i % n] = res[i];
+        }
+
+        return grid;
     }
 };
