92205fcfba2b7d08058ac9e4da38360ecd7d254f
Sai Teja Sree <ponnagantisaitejasree@gmail.com>
2026-04-23 18:08:05 +0530

    Time: 0 ms (100%), Space: 99.2 MB (67.63%) - LeetHub

---
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        int u1 = edges[0][0];
+        int v1 = edges[0][1];
+        
+        int u2 = edges[1][0];
+        int v2 = edges[1][1];
+        
+        // If u1 is present in the second edge, it's the center
+        if (u1 == u2 || u1 == v2) {
+            return u1;
+        }
+        
+        // Otherwise, v1 must be the center
+        return v1;
+    }
+};
