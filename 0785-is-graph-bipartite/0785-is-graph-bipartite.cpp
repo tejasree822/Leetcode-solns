1520a666fe2fedac624d14d5c05e9af34fb1766e
Sai Teja Sree <ponnagantisaitejasree@gmail.com>
2026-05-22 12:52:29 +0530

    Time: 0 ms (100%), Space: 17.8 MB (15.02%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,31 +1,42 @@
 class Solution {
-public:
-    bool dfs(int node, int currColor, vector<vector<int>>& graph, vector<int>& color) {
-        color[node] = currColor;
-
-        for (int nei : graph[node]) {
-           
-            if (color[nei] == 0) {
-                if (dfs(nei, -currColor, graph, color) == false) {
-                    return false;
-                }
-            } 
-            else if (color[nei] == currColor) {
-                return false;
+public: 
+bool bfs(int node,vector<vector<int>>& graph,vector<int>& vis,vector<int>& color){
+        queue<pair<int,int>> q;
+        q.push({node,1});
+        vis[node]=1;
+        color[node]=1;
+        while(!q.empty()){
+            int nodeq=q.front().first;
+            int colo=q.front().second;
+            q.pop();
+            for(auto it:graph[nodeq]){
+                 if(!vis[it]){
+                    vis[it]=1;
+                    if(colo==1){
+                        color[it]=0;
+                    }
+                    else{
+                        color[it]=1;
+                    }
+                    q.push({it,color[it]});
+                 }
+                 else{
+                    if(colo==color[it]) return false;
+                 }
             }
         }
         return true;
-    }
-
+}
     bool isBipartite(vector<vector<int>>& graph) {
-        int m = graph.size();
-       
-        vector<int> color(m, 0);
-
-        for (int i = 0; i < m; i++) {
-            if (color[i] == 0) {
-                if (dfs(i, 1, graph, color) == false) {
-                    return false;
+        int m=graph.size();
+        vector<int> vis(m,0);
+        queue<pair<int,int>> q;
+        vector<int>color(m,0);
+        
+        for(int i=0;i<m;i++){
+            if(!vis[i]){
+                if(!bfs(i,graph,vis,color))
+                {return false;
                 }
             }
         }
