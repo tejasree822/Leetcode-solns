485d523830ea673a880fc983f0937c21b65f0cc0
Sai Teja Sree <ponnagantisaitejasree@gmail.com>
2026-05-22 12:23:19 +0530

    Time: 0 ms (100%), Space: 14.6 MB (47.25%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,42 +1,63 @@
 class Solution {
 public:
-    void dfs(int r, int c, int m, int n, vector<vector<char>>& board) {
-      
-        if (r < 0 || r >= m || c < 0 || c >= n || board[r][c] != 'O') {
-            return;
-        }
-
-        board[r][c] = '#';
-
-        int dr[] = {1, -1, 0, 0};
-        int dc[] = {0, 0, 1, -1};
-
-        for (int i = 0; i < 4; i++) {
-            dfs(r + dr[i], c + dc[i], m, n, board);
-        }
-    }
-
     void solve(vector<vector<char>>& board) {
-        if (board.empty()) return;
-        int m = board.size();
-        int n = board[0].size();
-        for (int i = 0; i < m; i++) {
-            if (board[i][0] == 'O') dfs(i, 0, m, n, board);
-            if (board[i][n - 1] == 'O') dfs(i, n - 1, m, n, board);
-        }
-        for (int j = 0; j < n; j++) {
-            if (board[0][j] == 'O') dfs(0, j, m, n, board);
-            if (board[m - 1][j] == 'O') dfs(m - 1, j, m, n, board);
-        }
+        int m=board.size();
+        int n=board[0].size();
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == 'O') {
-                    board[i][j] = 'X'; 
-                } else if (board[i][j] == '#') {
-                    board[i][j] = 'O'; 
+       queue<pair<int,int>> q;
+
+        for(int i=0;i<m;i++){
+       if(board[i][0]=='O')
+       {q.push({i,0});
+      
+       board[i][0]='#';
+       }
+       if(board[i][n-1]=='O')
+        {q.push({i,n-1});
+     
+        board[i][n-1]='#';
+        }
+        }
+        for(int j=0;j<n;j++){
+       if(board[0][j]=='O')
+       {q.push({0,j});
+      
+       board[0][j]='#';
+       }
+       if(board[m-1][j]=='O')
+        {q.push({m-1,j});
+
+        board[m-1][j]='#';
+        }
+        }
+        int dr[]={-1,1,0,0};
+        int dc[]={0,0,-1,1};
+        while(!q.empty()){
+            int r=q.front().first;
+            int c=q.front().second;
+            q.pop();
+            for(int i=0;i<4;i++){
+                int newr=r+dr[i];
+                int newc=c+dc[i];
+                if(newr>=0&&newr<m&&newc>=0&&newc<n&&board[newr][newc]=='O'){
+                    q.push({newr,newc});
+                    board[newr][newc]='#';
+                    
+
+                }
+            }
+            
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]=='#'){
+                    board[i][j]='O';
+                }
+                else if(board[i][j]=='O'){
+                    board[i][j]='X';
                 }
             }
         }
+       
     }
 };
