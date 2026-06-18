c9a71560b806f066620416edf0110ce726883fcc
Sai Teja Sree <ponnagantisaitejasree@gmail.com>
2026-06-18 13:04:24 +0530

    Time: 0 ms (100%), Space: 10.7 MB (44.15%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-int f(int ind,vector<int>& nums,vector<int>& dp){
-    if(ind==0) return nums[ind];
-    if(ind<0) return 0;
-    if(dp[ind]!=-1) return dp[ind];
-    int pick=nums[ind]+f(ind-2,nums,dp);
-    int notpick=f(ind-1,nums,dp);
-    return dp[ind]=max(pick,notpick);
-}
+
     int rob(vector<int>& nums) {
         int n=nums.size();
+    
         vector<int> dp(n,-1);
-        dp[0]=0;
-        return f(n-1,nums,dp);
+        dp[0]=nums[0];
+        int neg=0;
+        for(int i=1;i<n;i++){
+            
+            int take=nums[i];
+            if(i>1) take+=dp[i-2];
+           
+            int nottake=dp[i-1];
+            dp[i]=max(take,nottake);
+        }
+        return dp[n-1];
 
      }
 };
