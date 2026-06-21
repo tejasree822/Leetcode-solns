cca08ba239822d0681ca724a1f97827f9b273201
Sai Teja Sree <ponnagantisaitejasree@gmail.com>
2026-06-21 11:31:13 +0530

    Time: 130 ms (54.81%), Space: 13.5 MB (80%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -9,30 +9,33 @@ class Solution {
             sum+=nums[i];
         }
         int target=(sum*0.5);
-        vector<vector<bool>> dp(n,vector<bool>(target+1,false));
+       vector<bool> prev(target+1,false);
+       vector<bool> curr(target+1,false);
         if((sum%2)!=0){
             return false;
         }
-        else{
-             for(int i=0;i<n;i++){
-                dp[i][0]=true;
-             }
+        
+       
+        else{ 
+             prev[0]=true;
+            
              if(nums[0] <= target) 
-               { dp[0][nums[0]]=true;}
+               { prev[nums[0]]=true;}
                 for(int i=1;i<n;i++){
                     for(int j=1;j<=target;j++){
-                        bool notpick=dp[i-1][j];
+                        bool notpick=prev[j];
                      bool pick=false;
                       if(j>=nums[i]){
-                         pick=dp[i-1][j-nums[i]];
+                         pick=prev[j-nums[i]];
                           }
-                          dp[i][j]=pick||notpick;
+                          curr[j]=pick||notpick;
                     }
+                    prev=curr;
                 }
              
 
         }
-        return dp[n-1][target];
+        return prev[target];
         
     }
 };
