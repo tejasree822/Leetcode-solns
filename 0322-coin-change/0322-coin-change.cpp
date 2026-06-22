8d77a5055b2d5797a7017d7822b6202fe9bcf6d9
Sai Teja Sree <ponnagantisaitejasree@gmail.com>
2026-06-22 13:19:48 +0530

    Time: 35 ms (50.84%), Space: 22.2 MB (45.7%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
- int f(int ind,int T,vector<int> &coins,vector<vector<int>>& dp){
-      if(ind==0){
-          if(T%coins[0]==0){
-              return T/coins[0];
-          }
-          else{
-              return 1e9;
-          }
-      }
-      if(dp[ind][T]!=-1) return dp[ind][T];
-      int nottake=0+f(ind-1,T,coins,dp);
-      int take=1e9;
-      if(coins[ind]<=T){
-          take=1+f(ind,T-coins[ind],coins,dp);
-      }
-      return dp[ind][T]=min(take,nottake);
-  }
     int coinChange(vector<int>& coins, int amount) {
         if(amount==0) return 0;
      int n=coins.size();
-     vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        int ans=f(n-1,amount,coins,dp);
+     vector<int> prev(amount+1,0),curr(amount+1,0);
+     
+    for(int T=0;T<=amount;T++){
+        if(T%coins[0]==0) prev[T]=T/coins[0];
+        else prev[T]=1e9;
+    }
+    for(int ind=1;ind<n;ind++){
+        for(int T=0;T<=amount;T++){
+            int nottake=0+prev[T];
+      int take=1e9;
+      if(coins[ind]<=T){
+          take=1+curr[T-coins[ind]];
+          
+      }
+      curr[T]=min(take,nottake);
+        }
+        prev=curr;
+    }
+     
+        int ans=prev[amount];
         if(ans>=1e9){
             return -1;
         }
