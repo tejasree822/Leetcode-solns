41eab38acae31900a20efca0e23d37129334e83f
Sai Teja Sree <ponnagantisaitejasree@gmail.com>
2026-06-22 15:14:12 +0530

    Time: 6 ms (69.32%), Space: 15.4 MB (52.15%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,32 +1,39 @@
 class Solution {
 public:
-     int f(int ind,int target,vector<int>& nums,vector<vector<int>>& dp,int offset){
-        if(target<-offset||target>offset)
-            return 0;
-       if(ind==0){
-        int cnt=0;
-        if(target==nums[0]){
-            cnt++;
-        }
-            if(target==(-nums[0])){
-               cnt++;
-            } 
-        return cnt;
-       }
-      if(dp[ind][target+offset]!=-1) return dp[ind][target+offset];
-        int plus=f(ind-1,target-nums[ind],nums,dp,offset);
-        int minus=f(ind-1,target+nums[ind],nums,dp,offset);
-        return dp[ind][target+offset]=plus+minus;
-     }
-
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n=nums.size();
-       int sum=0;
-       for(int num:nums){
-        sum+=num;
-       }
-       vector<vector<int>> dp(n,vector<int>(2*sum+1,-1));
-       
-        return f(n-1,target,nums,dp,sum);
+         int n=nums.size();
+         int totalSum=0;
+         for (int x:nums)
+            totalSum+=x;
+            if(abs(target)>totalSum)
+            return 0;
+        if((totalSum+target)%2!=0)
+            return 0;
+             int req=(totalSum+target)/2;
+         vector<vector<int>> dp(n,vector<int>(req+1,0));
+         if (nums[0]==0)
+            dp[0][0]=2;
+        else {
+            dp[0][0]=1;
+
+            if (nums[0]<=req)
+                dp[0][nums[0]]=1;
+        }
+
+        for (int ind = 1; ind < n; ind++) {
+
+            for (int sum = 0; sum <= req; sum++) {
+
+                int notTake = dp[ind - 1][sum];
+
+                int take = 0;
+                if (nums[ind] <= sum)
+                    take = dp[ind - 1][sum - nums[ind]];
+
+                dp[ind][sum] = take + notTake;
+            }
+        }
+
+        return dp[n - 1][req];
     }
 };
