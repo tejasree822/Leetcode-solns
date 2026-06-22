bfcd135a34c988a05735c76f9f7562f8fcf6d0dd
Sai Teja Sree <ponnagantisaitejasree@gmail.com>
2026-06-22 16:07:42 +0530

    Time: 39 ms (34.41%), Space: 61.2 MB (13.61%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
-int f(int ind,int T,vector<int>& coins,vector<vector<int>>& dp){
-if(ind==0){
-    return (T%coins[0]==0);
-}
-if(dp[ind][T]!=-1) return dp[ind][T];
-int nottake=f(ind-1,T,coins,dp);
-int take=0;
-if(coins[ind]<=T){
-    take=f(ind,T-coins[ind],coins,dp);
-}
-return dp[ind][T]=take+nottake;
-}
     int change(int amount, vector<int>& coins) {
-        int n=coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-         return f(n-1,amount,coins,dp);
+
+        int n = coins.size();
+
+        vector<vector<unsigned long long>> dp(
+            n,
+            vector<unsigned long long>(amount + 1, 0)
+        );
+
+        for (int T = 0; T <= amount; T++) {
+            dp[0][T] = (T % coins[0] == 0);
+        }
+
+        for (int ind = 1; ind < n; ind++) {
+            for (int T = 0; T <= amount; T++) {
+
+                unsigned long long nottake = dp[ind - 1][T];
+
+                unsigned long long take = 0;
+
+                if (coins[ind] <= T) {
+                    take = dp[ind][T - coins[ind]];
+                }
+
+                dp[ind][T] = take + nottake;
+            }
+        }
+
+        return (int)dp[n - 1][amount];
     }
 };
