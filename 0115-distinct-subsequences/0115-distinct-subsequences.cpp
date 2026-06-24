56f538cb30a42ca0c0ed69dc2e60e30287139fc7
Sai Teja Sree <ponnagantisaitejasree@gmail.com>
2026-06-24 15:14:14 +0530

    Time: 7 ms (96.09%), Space: 9 MB (91.01%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -4,20 +4,18 @@ public:
     int numDistinct(string s, string t) {
         int n=s.size();
         int m=t.size();
-        vector<unsigned long long> prev(m+1,0),curr(m+1,0);
+        vector<unsigned long long> prev(m+1,0);
         prev[0]=1;
         
         for(int i=1;i<=n;i++){
-             curr[0]=1;
-            for(int j=1;j<=m;j++){
+          
+            for(int j=m;j>=1;j--){
                 if(s[i-1]==t[j-1]){
-       curr[j]=prev[j]+prev[j-1];
+       prev[j]=prev[j]+prev[j-1];
     }
-    else{
-        curr[j]=prev[j];
-    }
+    
             }
-            prev=curr;
+            
         }
         return prev[m];
     }
