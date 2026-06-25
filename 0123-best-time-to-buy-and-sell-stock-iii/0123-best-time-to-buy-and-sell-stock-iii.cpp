e109923c3a422dec7ec82f578638cde9ed429319
Sai Teja Sree <ponnagantisaitejasree@gmail.com>
2026-06-25 12:21:04 +0530

    Time: 35 ms (71.27%), Space: 79.3 MB (76.89%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -3,19 +3,21 @@ public:
 
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<int>> dp(n+1,vector<int>(5,0));
+        vector<int> after(5,0);
+        vector<int>curr(5,0);
         for(int i=n-1;i>=0;i--){
             for(int tno=0;tno<=3;tno++){
                 if(tno%2==0){
-        dp[i][tno]=max(-prices[i]+dp[i+1][tno+1],dp[i+1][tno]);
+        curr[tno]=max(-prices[i]+after[tno+1],after[tno]);
     }
     else{
-        dp[i][tno]=max(prices[i]+dp[i+1][tno+1],dp[i+1][tno]);
+        curr[tno]=max(prices[i]+after[tno+1],after[tno]);
     }
             }
+            after=curr;
         }
 
-        return dp[0][0];
+        return after[0];
 
     }
 };
