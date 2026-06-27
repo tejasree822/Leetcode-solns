3cd908f37f337020d20d7a4e0b0e99935ec4d22c
Sai Teja Sree <ponnagantisaitejasree@gmail.com>
2026-06-27 13:49:47 +0530

    Time: 33 ms (89.52%), Space: 22.5 MB (44.73%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,43 +1,23 @@
 class Solution {
 public:
-bool check(string &s1,string &s2){
-    if(s1.size()!=s2.size()+1) return false;
-    int i=0,j=0;
-     while(i<s1.size()){
-        if(j<s2.size() && s1[i]==s2[j]){
-            i++;j++;
-        }
-        else{
-            i++;
-        }
-     }
-     if(i==s1.size() &&j==s2.size()) return true;
-     return false;
-}
     int longestStrChain(vector<string>& words) {
-         int n=words.size();
-         vector<int> dp(n,1);
-      sort(words.begin(), words.end(),
-     [](const string &a,const string &b) {
-         return a.size()<b.size();
+        sort(words.begin(), words.end(),
+     [](string &a, string &b){
+         return a.size() < b.size();
      });
-                int maxi=1;
-        int lastIndex=0;
-        for(int i=0;i<n;i++){
-        
-        for(int prev=0;prev<i;prev++){
-      if(check(words[i],words[prev]) && 1+dp[prev]>dp[i]){
-         dp[i]=1+dp[prev];
-         
-      }
-        }
-        if(dp[i]>maxi){
-            maxi=dp[i];
-            lastIndex=i;
-        }
-        }
-        return maxi;
-      
 
+     unordered_map<string,int> dp;
+      int ans=1;
+  for(string word:words){
+    dp[word] = 1;
+  for(int i=0;i<word.size();i++){
+   string prev=word.substr(0,i)+word.substr(i+1);
+     if(dp.count(prev))
+            dp[word]=max(dp[word],dp[prev]+1);
+    }
+
+    ans = max(ans,dp[word]);
+}
+return ans;
     }
 };
