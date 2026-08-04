54dc610088964841f058e3eb32118d6f4e384484
Sai Teja Sree <ponnagantisaitejasree@gmail.com>
2026-08-04 22:53:28 +0530

    Time: 3 ms (49.33%), Space: 33.7 MB (17.56%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        int a=nums[0];
+        int b=nums[0];
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]<a){
+                a=nums[i];
+            }
+            if(nums[i]>b){
+                b=nums[i];
+            }
+        }
        vector<int> ans;
        unordered_set<int> mp;
        for(int x:nums){
         mp.insert(x);
        }
-       int a=nums[0];
-       int b=nums[nums.size()-1];
        int k=a+1;
        while(k!=b){
         if(mp.find(k)==mp.end()){
